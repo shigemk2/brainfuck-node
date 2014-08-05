2587ede17e213286348c840a6a4afbd92285a420
7shi <7shi@live.jp>
2014-08-05 09:57:40 +0900

    support indent for bftranslate-ffi

--- bftranslate-ffi.js
+++ bftranslate-ffi.js
@@ -1,49 +1,56 @@
+var indent = "", data = "";
+
+function write(line) {
+  data += indent + line + "\n";
+}
+
 function main(src) {
-  var data = "", pc = 0;
-  data += "var ffi = require('ffi');\n";
-  data += "var libcName = process.platform == 'win32' ? 'msvcrt' : 'libc';\n";
-  data += "var libc = ffi.Library(libcName, {\n";
-  data += "  'putchar': ['int', ['int']],\n";
-  data += "  'getchar': ['int', []],\n";
-  data += "});\n";
-  data += "var mem = new Uint8Array(30000);\n";
-  data += "var r = 0;\n";
+  var pc = 0;
+  write("var ffi = require('ffi');");
+  write("var libcName = process.platform == 'win32' ? 'msvcrt' : 'libc';");
+  write("var libc = ffi.Library(libcName, {");
+  write("  'putchar': ['int', ['int']],");
+  write("  'getchar': ['int', []],");
+  write("});");
+  write("var mem = new Uint8Array(30000);");
+  write("var r = 0;");
   while( pc <= src.length ) {
     // console.log(src[pc]);
     switch (src[pc]) {
     case '+':
-      data += "mem[r]++; /* + */\n";
+      write("mem[r]++; /* + */");
       break;
     case '-':
-      data += "mem[r]--; /* - */\n";
+      write("mem[r]--; /* - */");
       break;
     case '>':
-      data += "r++; /* > */\n";
+      write("r++; /* > */");
       break;
     case '<':
-      data += "r--; /* < */\n";
+      write("r--; /* < */");
       break;
     case '[':
-      data += "while (mem[r]) { /* [ */\n";
+      write("while (mem[r]) { /* [ */");
+      indent += "  ";
       break;
     case ']':
-      data += "} /* ] */\n";
+      indent = indent.slice(0, -2);
+      write("} /* ] */");
       break;
     case '.':
-      data += "libc.putchar(mem[r]); /* . */\n";
+      write("libc.putchar(mem[r]); /* . */");
       break;
     case ',':
-      data += "mem[r] = libc.getchar(); /* , */\n";
+      write("mem[r] = libc.getchar(); /* , */");
       break;
     }
     pc++;
   }
-  return data;
 }
 
 var fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, src) {
-  var data = main(src);
+  main(src);
   fs.writeFile(process.argv[3], data , function (err) {
     // console.log(data);
     // console.log(err);
