bcf43caf824b44402eb012b0d0d614b57fd150b9
shigemk2 <i.am.shige@gmail.com>
2014-08-05 21:58:00 +0900

    Eval ffi

---
+++ bfeval-ffi.js
@@ -0,0 +1,54 @@
+var indent = "", data = "";
+
+function write(line) {
+  data += indent + line + "\n";
+  return data;
+}
+
+function main(src) {
+  write("var ffi = require('ffi');");
+  write("var libcName = process.platform == 'win32' ? 'msvcrt' : 'libc';");
+  write("var libc = ffi.Library(libcName, {");
+  write("  'putchar': ['int', ['int']],");
+  write("  'getchar': ['int', []],");
+  write("});");
+  write("var mem = new Uint8Array(30000);");
+  write("var r = 0;");
+  for (var pc = 0; pc < src.length; pc++) {
+    // console.log(src[pc]);
+    switch (src[pc]) {
+    case '+':
+      write("mem[r]++; // +");
+      break;
+    case '-':
+      write("mem[r]--; // -");
+      break;
+    case '>':
+      write("r++; // >");
+      break;
+    case '<':
+      write("r--; // <");
+      break;
+    case '[':
+      write("while (mem[r]) { // [");
+      indent += "  ";
+      break;
+    case ']':
+      indent = indent.slice(0, -2);
+      write("} // ]");
+      break;
+    case '.':
+      write("libc.putchar(mem[r]); // .");
+      break;
+    case ',':
+      write("mem[r] = libc.getchar(); // ,");
+      break;
+    }
+  }
+  return data;
+}
+
+var fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, src) {
+  eval("(function() {" + main(src) + "})();");
+});
